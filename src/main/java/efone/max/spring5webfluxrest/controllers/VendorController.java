c598b909b1dafabc5769de173c77e377af4bba7f
efone <gl.maksimal@gmail.com>
2020-08-24 22:26:45 +0500

    417-418 done

--- src/main/java/efone/max/spring5webfluxrest/controllers/VendorController.java
+++ src/main/java/efone/max/spring5webfluxrest/controllers/VendorController.java
@@ -2,27 +2,49 @@ package efone.max.spring5webfluxrest.controllers;
 
 import efone.max.spring5webfluxrest.domain.Vendor;
 import efone.max.spring5webfluxrest.repositories.VendorRepository;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping(VendorController.URL)
 public class VendorController {
+    public static final String URL = "/api/v1/vendors";
     private final VendorRepository repository;
 
     public VendorController(VendorRepository repository) {
         this.repository = repository;
     }
 
-    @GetMapping("/api/v1/vendors")
+    @GetMapping
     public Flux<Vendor> listAll() {
         return repository.findAll();
     }
 
-    @GetMapping("/api/v1/vendors/{id}")
+    @GetMapping("{id}")
     public Mono<Vendor> getById(@PathVariable String id) {
         return repository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    Mono<Void> create(@RequestBody Publisher<Vendor> vendorPublisher) {
+        return repository.saveAll(vendorPublisher).then();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("{id}")
+    Mono<Vendor> update(@PathVariable String id, @RequestBody Vendor vendor) {
+        vendor.setId(id);
+        return repository.save(vendor);
     }
 }
--- src/test/java/efone/max/spring5webfluxrest/controllers/VendorControllerTest.java
+++ src/test/java/efone/max/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -8,11 +8,13 @@ import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -30,7 +32,7 @@ public class VendorControllerTest {
     }
 
     @Test
-    public void listAll() {
+    public void testListAll() {
         BDDMockito.given(repository.findAll()).willReturn(Flux.just(Vendor.builder().firstname("FirstFirstName").build(),
                 Vendor.builder().firstname("SecondFirstNameBuild").build()));
 
@@ -41,12 +43,41 @@ public class VendorControllerTest {
     }
 
     @Test
-    public void getById() {
+    public void testGetById() {
         BDDMockito.given(repository.findById(anyString())).willReturn(
                 Mono.just(Vendor.builder().firstname("FirstFirstName").build()));
 
         webTestClient.get().uri("/api/v1/vendors/bla")
                 .exchange()
                 .expectBody(Vendor.class);
+    }
+
+    @Test
+    public void testCreate() {
+        Flux<Vendor> vendors = Flux.just(Vendor.builder().firstname("FirstFirstName").build(),
+                Vendor.builder().firstname("SecondFirstName").build());
+
+        BDDMockito.given(repository.saveAll(any(Publisher.class))).willReturn(
+                vendors);
+
+        webTestClient.post().uri(VendorController.URL)
+                .body(vendors, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+    @Test
+    public void testUpdate() {
+        Mono<Vendor> firstVendor = Mono.just(Vendor.builder().firstname("FirstFirstName").build());
+
+        BDDMockito.given(repository.save(any(Vendor.class))).willReturn(
+                firstVendor);
+
+        webTestClient.put().uri(VendorController.URL + "/324234d")
+                .body(firstVendor, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
     }
 }
